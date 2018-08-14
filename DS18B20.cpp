e553af618106b4d5790fa97e1b297d8c21ef004a
hhgyu <hhgyu15@gmail.com>
2018-08-14 23:37:13 +0900

    first commit

---
+++ DS18B20.cpp
@@ -0,0 +1,55 @@
+#include "pxt.h"
+
+#include "OneWire.h"
+
+using namespace pxt;
+
+enum class Pins {
+  P0 =  3,
+  P1 =  2,
+  P2 =  1,
+  P3 =  4,
+  P4 =  5,
+  P5 =  17,
+  P6 =  12,
+  P7 =  11,
+  P8 =  18,
+  P9 =  10,
+  P10 = 6,
+  P11 = 26,
+  P12 = 20,
+  P13 = 23,
+  P14 = 22,
+  P15 = 21,
+  P16 = 16,
+  P19 = 0,
+  P20 = 30
+};
+
+namespace DS18B20 {
+  OneWire * mOneWire = NULL;
+  
+  //% blockId=probe_init
+  //% block="connect temperature probe to %pin"
+  void init(Pins pin) {
+    if (mOneWire != NULL) {
+      delete mOneWire;
+    }
+  
+    mOneWire = new OneWire((PinName) pin);
+  
+    mOneWire->init();
+  }
+  
+  //% blockId = get_temp
+  //% block="temperature"
+  float get_temp() {
+    if (mOneWire == NULL)
+      return -1000;
+  
+    rom_address_t address;
+    mOneWire->singleDeviceReadROM(address);
+    mOneWire->convertTemperature(address, true, true); //Start temperature conversion, wait until ready
+    return mOneWire->temperature(address);
+  }
+}
---
+++ shims.d.ts
@@ -0,0 +1,22 @@
+// Auto-generated. Do not edit.
+
+
+    /**
+     * hhgyu
+     * 2018-08-14
+     * https://github.com/hhgyu/pxt-ds18b20-temperature-sensor
+     *
+     * Development environment specifics:
+     * Written in Microsoft PXT
+     * Tested with a pxt-ds18b20-temperature-sensor for micro:bit
+     *
+     * This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+     * Please review the LICENSE.md file included with this example. If you have any questions 
+     * or concerns with licensing, please contact hhgyu15@gmail.com.
+     * Distributed as-is; no warranty is given.
+     */
+
+declare namespace ds18b20 {
+}
+
+// Auto-generated. Do not edit. Really.
